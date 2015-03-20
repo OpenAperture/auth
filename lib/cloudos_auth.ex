caec2cb5ef519e2730664a4a19009861b55c9357
Mike Binns <mike.binns@perceptivesoftware.com>
2015-03-20 12:54:56 -0400

    updated validation call

--- lib/cloudos_auth.ex
+++ lib/cloudos_auth.ex
@@ -78,43 +78,35 @@ defmodule CloudosAuth do
   end
 
   @doc """
-  Method to validate a Google OAuth authentication header
+  Method to validate an OAuth token
   ## Options
   The `auth_header` option defines the auth header
   ## Return values
   Boolean
   """
- @spec validate_header(pid, String.t()) :: :ok | :error
-  def validate_header(pid, auth_header) do
-    if (!String.starts_with?(auth_header, "OAuth ")) do
-      false
-    else
-      options = Agent.get(pid, fn options -> options end)
-
-      access_token = to_string(tl(String.split(auth_header, "OAuth ")))
-
-      url = "#{options[:url]}/info?#{access_token}"
-      Logger.debug("Executing OAuth call:  #{url}")
-      try do
-        case :httpc.request(:get, {url, [{'Accept', 'application/json'}]}, [], []) do
-          {:ok, {{_,return_code, _}, _, body}} ->
-            case return_code do
-              200 -> 
-                Logger.debug("Received response from OAuth:  #{inspect body}")
-                userinfo_json = JSON.decode!("#{body}")
-                Logger.debug("Parsed OAuth response:  #{inspect userinfo_json}")
-                cond do
-                  userinfo_json["expires_in_seconds"] == nil || userinfo_json["expires_in_seconds"] <= 0 -> false
-                  true -> true
-                end
-              _   -> false
-            end
-          {:error, {failure_reason, _}} -> false
-        end
-      rescue e in _ ->
-        Logger.error("An error occurred calling OAuth:  #{inspect e}")
-        false 
-      end
-    end
-  end 
+ @spec validate_token(String.t(), String.t()) :: :ok | :error
+  def validate_token(validate_url, token) do
+   	url = "#{validate_url}?#{token}"
+		Logger.debug("Executing OAuth call:  #{url}")
+		try do
+	    case :httpc.request(:get, {url, [{'Accept', 'application/json'}]}, [], []) do
+	      {:ok, {{_,return_code, _}, _, body}} ->
+	        case return_code do
+	          200 -> 
+	            Logger.debug("Received response from OAuth:  #{inspect body}")
+	            userinfo_json = JSON.decode!("#{body}")
+	            Logger.debug("Parsed OAuth response:  #{inspect userinfo_json}")
+	            cond do
+	              userinfo_json["expires_in_seconds"] == nil || userinfo_json["expires_in_seconds"] <= 0 -> false
+	              true -> true
+	            end
+	          _   -> false
+	        end
+	      {:error, {failure_reason, _}} -> false
+	    end
+	  rescue e in _ ->
+	    Logger.error("An error occurred calling OAuth:  #{inspect e}")
+	    false 
+	  end
+	end
 end
