abac95413eea6b9884d7b5cefb873e2cd35eb597
Mike Binns <mike.binns@perceptivesoftware.com>
2015-03-20 10:50:42 -0400

    agent changes

--- lib/cloudos_auth.ex
+++ lib/cloudos_auth.ex
@@ -9,9 +9,8 @@ defmodule CloudosAuth.Client do
 
   @spec create() :: {:ok, pid} | {:error, String.t()} 
   def create() do
-    case Agent.start_link(fn -> %{} end) do
+    case Agent.start_link(__MODULE__, fn -> %{} end) do
       {:ok, pid} -> 
-        get_token(true)
         {:ok, pid}
       {:error, reason} -> {:error, reason}
     end
@@ -24,16 +23,16 @@ defmodule CloudosAuth.Client do
   ## Return values
   String
   """
-  @spec get_token(pid, term) :: String.t()
-  def get_token(pid, force_refresh \\ false) do
-    options = Agent.get(pid, fn options -> options end)
+  @spec get_token(String.t(), String.t(), String.t(), term) :: String.t()
+  def get_token(url, client_id, client_secret, force_refresh \\ false) do
+    options = Agent.get(__MODULE__, fn options -> options end)
 
     cond do
-      options[:auth_token] != nil && !force_refresh -> options[:auth_token]
+      options[url] != nil && !force_refresh -> options[url]
       true ->
-        options = Map.put(options, :auth_token, get_token_raw())
-        Agent.update(pid, fn _ -> options end)
-        options[:auth_token]
+        options = Map.put(options, url, get_token_raw(url, client_id, client_secret))
+        Agent.update(__MODULE__, fn _ -> options end)
+        options[url]
     end
   end
 
@@ -42,14 +41,14 @@ defmodule CloudosAuth.Client do
   ## Return values
   String
   """
-  @spec get_token_raw() :: String.t()
-  def get_token_raw() do
+  @spec get_token_raw(String.t(), String.t(), String.t()) :: String.t()
+  def get_token_raw(url, client_id, client_secret) do
     body = '#{JSON.encode!(%{
       grant_type: "client_credentials", 
-      client_id: Application.get_env(:cloudos_auth, :client_id), 
-      client_secret: Application.get_env(:cloudos_auth, :client_secret)
+      client_id: client_id, 
+      client_secret: client_secret
     })}'
-    case :httpc.request(:post, {'#{Application.get_env(:cloudos_auth, :login_url)}', [{'Content-Type', 'application/json'}, {'Accept', 'application/json'}], 'application/json', body}, [], []) do
+    case :httpc.request(:post, {url, [{'Content-Type', 'application/json'}, {'Accept', 'application/json'}], 'application/json', body}, [], []) do
       {:ok, {{_,return_code, _}, _, body}} ->
         case return_code do
           200 -> 
@@ -72,8 +71,8 @@ defmodule CloudosAuth.Client do
   ## Return values
   String
   """
-  @spec get_auth_header(term) :: String.t()
-  def get_auth_header(force_refresh \\ false) do
-    "OAuth access_token=#{get_token(force_refresh)}"
+  @spec get_auth_header(String.t(), String.t(), String.t(), term) :: String.t()
+  def get_auth_header(url, client_id, client_secret, force_refresh \\ false) do
+    "OAuth access_token=#{get_token(url, client_id, client_secret, force_refresh)}"
   end
 end
